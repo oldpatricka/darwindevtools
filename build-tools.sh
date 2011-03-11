d581c983ee76c2209d7ca304647eb4b555836b2a
oldpatricka <patricka@uvic.ca>
2011-03-10 21:19:31 -0800

    Flesh out scripts

--- build-tools.sh
+++ build-tools.sh
@@ -29,4 +29,8 @@ get_tarball ${APPLE_GCC_URL}
 GCC_TARBALL=`basename ${APPLE_GCC_URL}`
 untar ${GCC_TARBALL}
 GCC_DIR=`echo ${GCC_TARBALL} | sed 's/.tar.gz//'`
+cd ${GCC_DIR}
 
+# From README.Apple:
+mkdir -p build/obj build/dst build/sym
+gnumake install RC_OS=macos RC_ARCHS='ppc i386' TARGETS='i386 ppc' SRCROOT=`pwd` OBJROOT=`pwd`/build/obj DSTROOT=`pwd`/build/dst SYMROOT=`pwd`/build/sym
