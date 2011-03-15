befeee886adf942c4524c3d09f400d4d100364d2
oldpatricka <patricka@uvic.ca>
2011-03-14 21:08:09 -0700

    Added some tests, have gnumake building as well, now.

---
+++ build-test.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+PREFIX=./Distribution/Package_root
+TESTS=./tests
+
+GCC_TEST=${TESTS}/gcc4.2.pkg.contents
+DEVTOOLS_TEST=${TESTS}/DeveloperToolsCLI.pkg.contents
+
+
+echo "Test GCC..."
+for file in `cat ${GCC_TEST}` ; do
+
+        ls $PREFIX/$file >/dev/null 2>&1
+        EXISTS=$?
+        if [ $EXISTS -ne 0 ]; then
+            echo "$PREFIX/$file is missing"
+        fi
+done
+
+echo "Test DevToolsCLI..."
+for file in `cat ${DEVTOOLS_TEST}` ; do
+
+        ls $PREFIX/$file >/dev/null 2>&1
+        EXISTS=$?
+        if [ $EXISTS -ne 0 ]; then
+            echo "$PREFIX/$file is missing"
+        fi
+done
--- build-tools.sh
+++ build-tools.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 APPLE_GCC_URL=http://www.opensource.apple.com/tarballs/gcc/gcc-5646.tar.gz
+APPLE_GNUMAKE_URL=http://www.opensource.apple.com/tarballs/gnumake/gnumake-126.2.tar.gz
+APPLE_GDB_URL=http://www.opensource.apple.com/tarballs/gdb/gdb-1344.tar.gz
+APPLE_LIBC_URL=http://www.opensource.apple.com/tarballs/Libc/Libc-594.9.4.tar.gz
 
 
 ECHO=/bin/echo
-BUILD=build
-PREFIX=`pwd`/Distribution/Package_root/usr/local/
+BUILD=`pwd`/build
+PREFIX=`pwd`/Distribution/Package_root
 
 function get_tarball {
     tarball_url=$1
@@ -24,13 +27,36 @@ function untar {
 
 
 mkdir -p ${BUILD}
-cd ${BUILD}
-get_tarball ${APPLE_GCC_URL}
-GCC_TARBALL=`basename ${APPLE_GCC_URL}`
-untar ${GCC_TARBALL}
-GCC_DIR=`echo ${GCC_TARBALL} | sed 's/.tar.gz//'`
-cd ${GCC_DIR}
+
+
+# GCC
+#cd ${BUILD}
+#get_tarball ${APPLE_GCC_URL}
+#GCC_TARBALL=`basename ${APPLE_GCC_URL}`
+#untar ${GCC_TARBALL}
+#GCC_DIR=`echo ${GCC_TARBALL} | sed 's/.tar.gz//'`
+#cd ${GCC_DIR}
 
 # From README.Apple:
-mkdir -p build/obj build/dst build/sym
-gnumake install RC_OS=macos RC_ARCHS='ppc i386' TARGETS='i386 ppc' SRCROOT=`pwd` OBJROOT=`pwd`/build/obj DSTROOT=`pwd`/build/dst SYMROOT=`pwd`/build/sym
+#mkdir -p build/obj build/dst build/sym
+#gnumake install RC_OS=macos RC_ARCHS='ppc i386' TARGETS='i386 ppc' SRCROOT=`pwd` OBJROOT=`pwd`/build/obj DSTROOT=`pwd`/build/dst SYMROOT=`pwd`/build/sym
+#ditto build/dst $PREFIX
+
+# Make
+cd ${BUILD}
+get_tarball ${APPLE_GNUMAKE_URL}
+GNUMAKE_TARBALL=`basename ${APPLE_GNUMAKE_URL}`
+untar ${GNUMAKE_TARBALL}
+GNUMAKE_DIR=`echo ${GNUMAKE_TARBALL} | sed 's/.tar.gz//'`
+cd ${GNUMAKE_DIR}
+gnumake install
+ditto /tmp/gnumake/Release $PREFIX
+
+
+# GDB
+cd ${BUILD}
+get_tarball ${APPLE_GDB_URL}
+GDB_TARBALL=`basename ${APPLE_GDB_URL}`
+untar ${GDB_TARBALL}
+GDB_DIR=`echo ${GDB_TARBALL} | sed 's/.tar.gz//'`
+cd ${GDB_DIR}
